b878bd30d24315f05bab37cd77b30b4458156c89
asatriangrish <68015194+asatriangrish@users.noreply.github.com>
2022-08-26 21:25:55 +0400

    feat: gmail nodemailer

--- src/config/config.js
+++ src/config/config.js
@@ -50,10 +50,20 @@ module.exports = {
     resetPasswordExpirationMinutes: envVars.JWT_RESET_PASSWORD_EXPIRATION_MINUTES,
     verifyEmailExpirationMinutes: envVars.JWT_VERIFY_EMAIL_EXPIRATION_MINUTES,
   },
+  // email: {
+  //   smtp: {
+  //     host: envVars.SMTP_HOST,
+  //     port: envVars.SMTP_PORT,
+  //     auth: {
+  //       user: envVars.SMTP_USERNAME,
+  //       pass: envVars.SMTP_PASSWORD,
+  //     },
+  //   },
+  //   from: envVars.EMAIL_FROM,
+  // },
   email: {
     smtp: {
-      host: envVars.SMTP_HOST,
-      port: envVars.SMTP_PORT,
+      service: 'gmail',
       auth: {
         user: envVars.SMTP_USERNAME,
         pass: envVars.SMTP_PASSWORD,
