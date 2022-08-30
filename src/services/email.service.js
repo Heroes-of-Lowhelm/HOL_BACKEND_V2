c0708895352e6d00c4cc949098dcabc512f10b83
asatriangrish <68015194+asatriangrish@users.noreply.github.com>
2022-08-30 11:36:03 +0400

    fix: verify.then is not a fucntion

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -9,12 +9,12 @@ const transport = nodemailer.createTransport(
   })
 );
 /* istanbul ignore next */
-if (config.env !== 'test') {
-  transport
-    .verify()
-    .then(() => logger.info('Connected to email server'))
-    .catch(() => logger.warn('Unable to connect to email server. Make sure you have configured the SMTP options in .env'));
-}
+// if (config.env !== 'test') {
+//   transport
+//     .verify()
+//     .then(() => logger.info('Connected to email server'))
+//     .catch(() => logger.warn('Unable to connect to email server. Make sure you have configured the SMTP options in .env'));
+// }
 
 /**
  * Send an email
