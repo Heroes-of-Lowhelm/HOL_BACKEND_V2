d36e4d82d666c12e1aa26c405c8a03dc299e2fab
asatriangrish <68015194+asatriangrish@users.noreply.github.com>
2022-08-30 11:31:35 +0400

    feat: sendgrid SMTP server

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,8 +1,13 @@
 const nodemailer = require('nodemailer');
+const nodemailerSendgrid = require('nodemailer-sendgrid');
 const config = require('../config/config');
 const logger = require('../config/logger');
 
-const transport = nodemailer.createTransport(config.email.smtp);
+const transport = nodemailer.createTransport(
+  nodemailerSendgrid({
+    apiKey: process.env.SENDGRID_API_KEY,
+  })
+);
 /* istanbul ignore next */
 if (config.env !== 'test') {
   transport
